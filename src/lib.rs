a5e8f8887856372a9947e67362216fd9b35bd00d
Léo Gaspard <leo@gaspard.io>
2024-01-03 22:39:08 +0100

    document

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![doc = include_str!("../README.md")]
+
 use std::time::Duration;
 
 #[cfg(test)]
@@ -12,9 +14,25 @@ mod noop;
 #[cfg(not(test))]
 pub use noop::*;
 
+/// Configuration for a `reord`-based test
 #[derive(Debug)]
 pub struct Config {
+    /// The random seed used to choose which task to run
+    ///
+    /// Changing this seed will give other task orderings, but leaving it the same should (if
+    /// lock tracking is correctly implemented by the application using `Lock`) keep execution
+    /// reproducible.
     pub seed: [u8; 32],
+
+    /// If set to `Some`, will allow two tasks to voluntarily collide on a named lock to validate
+    /// that locking is implemented correctly. It will then wait for the time indicated by this
+    /// setting, and if the next `reord::point` has not been reached by then, assume the locking
+    /// worked properly and continue testing.
     pub check_named_locks_work_for: Option<Duration>,
+
+    /// If set to `Some`, will allow two tasks to voluntarily collide on an addressed lock to
+    /// validate that locking is implemented correctly. It will then wait for the time indicated
+    /// by this setting, and if the next `reord::point` has not been reached by then, assume the
+    /// locking worked properly and continue testing.
     pub check_addressed_locks_work_for: Option<Duration>,
 }
--- src/noop.rs
+++ src/noop.rs
@@ -1,34 +1,61 @@
+#![allow(unused_variables)]
+
 use crate::Config;
 use std::future::Future;
 
+/// Start a test
+///
+/// Note that this relies on global variables for convenience, and thus should only ever be used with
+/// `cargo-nextest`.
 #[inline]
-pub async fn init_test(_: Config) {}
+pub async fn init_test(config: Config) {}
 
+/// Add a task to the `reord` framework
+///
+/// This should be used around all the futures spawned by the test
 #[inline]
 pub async fn new_task<T>(f: impl Future<Output = T>) -> T {
     f.await
 }
 
+/// Start the test
+///
+/// This should be called after at least one task has been spawned on the executor, wrapped by `new_task`.
+/// This will start executing tasks in a random but reproducible order.
 #[inline]
 pub async fn start() {}
 
+/// Execution order randomization point
+///
+/// Reaching this point makes `reord` able to switch the execution to another thread.
 #[inline]
 pub async fn point() {}
 
-/// Note: this should be dropped as soon as the underlying lock is released, and a `reord::point().await`
-/// should be inserted as soon as possible after that. This is required especially for lock checking,
-/// as lock checks need to have two tasks allowed to run at the same time. Similarly, just after the lock
-/// is acquired, you should have a `reord::point().await`.
+/// Lock handling
+///
+/// This records, for `reord`, which locks are currently taken by the program. The lock should be acquired
+/// as close as possible before the real lock acquiring, and released as soon as possible after the real
+/// lock release.
+///
+/// In addition, there should be a `reord::point().await` just after the real lock managed to be acquired,
+/// and one ideally just after the real lock was released, though this may be harder due to early returns
+/// and the current absence of `async Drop`.
+///
+/// If too long passes with `reord` unaware of the state of your locks, you could end up with
+/// non-reproducible behavior, due to two execution threads actually running in parallel on your executor.
 #[derive(Debug)]
 pub struct Lock(());
 
 impl Lock {
+    /// Take a lock with a given name
     #[inline]
-    pub async fn take_named(_: String) -> Lock {
+    pub async fn take_named(name: String) -> Lock {
         Lock(())
     }
+
+    /// Take a lock at a given address
     #[inline]
-    pub async fn take_addressed(_: usize) -> Lock {
+    pub async fn take_addressed(address: usize) -> Lock {
         Lock(())
     }
 }
